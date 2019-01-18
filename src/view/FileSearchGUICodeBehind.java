71e2d5097c42a1ca843b4335d90183cea2720a44
csuser <csuser@160.10.217.129>
2019-01-18 16:52:56 -0500

    CodeBehind needs to be linked to FXML

--- src/view/FileSearchGUICodeBehind.java
+++ src/view/FileSearchGUICodeBehind.java
@@ -3,11 +3,36 @@
  */
 package view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 /**
  * @author Jeremiah Liscum
  *
  */
 public class FileSearchGUICodeBehind {
-	
+	 	@FXML
+	    private AnchorPane chooseDirectoryButton;
+
+	    @FXML
+	    private TextField patternMatchTextFlied;
+
+	    @FXML
+	    private RadioButton selectAllRadioButton;
+
+	    @FXML
+	    private RadioButton directoriesOnlyRadioButton;
+
+	    @FXML
+	    private RadioButton filesOnlyRadioButton;
+
+	    @FXML
+	    private RadioButton onlyFilesNamesRadioButton;
+
+	    @FXML
+	    private RadioButton fullPathNames;
+
 
 }
