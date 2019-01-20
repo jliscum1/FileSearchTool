cac74204d38a98c0d45a3d5cc9ba55c7038882ad
jliscum1 <jliscum1@160.10.106.57>
2019-01-19 22:01:37 -0600

    Commit

--- src/edu/westga/cs3151/filesearch/view/FileSearchGUICodeBehind.java
+++ src/edu/westga/cs3151/filesearch/view/FileSearchGUICodeBehind.java
@@ -3,7 +3,11 @@
  */
 package edu.westga.cs3151.filesearch.view;
 
+import edu.westga.cs3211.time_management.model.Calendar;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -13,6 +17,34 @@ import javafx.scene.layout.AnchorPane;
  *
  */
 public class FileSearchGUICodeBehind {
-	 
+	  	@FXML
+	    private Button chooseDirectoryButton;
+
+	    @FXML
+	    private TextField patternMatchTextFlied;
+
+	    @FXML
+	    private RadioButton selectAllRadioButton;
+
+	    @FXML
+	    private RadioButton directoriesOnlyRadioButton;
+
+	    @FXML
+	    private RadioButton filesOnlyRadioButton;
+
+	    @FXML
+	    private RadioButton onlyFilesNamesRadioButton;
+
+	    @FXML
+	    private RadioButton fullPathNames;
+
+	    @FXML
+	    void handleChooseDirectory(ActionEvent event) {
+
+	    }
+	    @FXML
+	    void initialize() {
+	        
+	    }
 
 }
