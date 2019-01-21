683c9991edde53437b61630bb07cc8803d237dd4
jliscum1 <jliscum1@160.10.105.27>
2019-01-21 11:59:36 -0600

    More Work needs to be done

--- src/edu/westga/cs3151/filesearch/controller/DirectoryTree.java
+++ src/edu/westga/cs3151/filesearch/controller/DirectoryTree.java
@@ -13,33 +13,33 @@ import javax.swing.JFileChooser;
  */
 public class DirectoryTree {
 
-	private static ArrayList<String> list;
-	
-	private static void addFiles(File dir) {
-		if(dir.isDirectory()) {
-			for(File file : dir.listFiles()) {
-				list.add(file.getAbsolutePath());
-				if(file.isDirectory()) {
+	private ArrayList<String> list;
+	private ArrayList<String> listOfFiles;
+
+	public DirectoryTree() {
+		this.list = new ArrayList<String>();
+		
+
+	}
+
+	public void addFiles(File dir) {
+		if (dir.isDirectory()) {
+			for (File file : dir.listFiles()) {
+				this.list.add(file.getAbsolutePath());
+				if (file.isDirectory()) {
 					addFiles(file);
 				}
 			}
 		}
+		
 	}
-	public static void main(String[] arg) {
-		
-		list = new ArrayList<String>();
-		JFileChooser chooser = new JFileChooser();
-		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-		chooser.setAcceptAllFileFilterUsed(false);
-		int value = chooser.showOpenDialog(null);
-		
-		if(value != JFileChooser.APPROVE_OPTION) {
-			return;
-		}
-		addFiles(chooser.getSelectedFile());
-		for(String name : list) {
-			System.out.println(name);
-		}
-		
+
+	public void clearList() {
+		this.list.clear();
+
+	}
+	
+	public ArrayList<String> getList() {
+		return this.list;
 	}
 }
--- src/edu/westga/cs3151/filesearch/view/FileSearchGUICodeBehind.java
+++ src/edu/westga/cs3151/filesearch/view/FileSearchGUICodeBehind.java
@@ -3,55 +3,69 @@
  */
 package edu.westga.cs3151.filesearch.view;
 
+import java.io.File;
 
 import edu.westga.cs3151.filesearch.controller.DirectoryTree;
-import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
+import javafx.stage.DirectoryChooser;
 
 /**
  * @author Jeremiah Liscum
  *
  */
 public class FileSearchGUICodeBehind {
-	  	@FXML
-	    private Button chooseDirectoryButton;
+	@FXML
+	private Button chooseDirectoryButton;
 
-	    @FXML
-	    private TextField patternMatchTextFlied;
+	@FXML
+	private TextField patternMatchTextFlied;
 
-	    @FXML
-	    private RadioButton selectAllRadioButton;
+	@FXML
+	private RadioButton selectAllRadioButton;
 
-	    @FXML
-	    private RadioButton directoriesOnlyRadioButton;
+	@FXML
+	private RadioButton directoriesOnlyRadioButton;
 
-	    @FXML
-	    private RadioButton filesOnlyRadioButton;
+	@FXML
+	private RadioButton filesOnlyRadioButton;
 
-	    @FXML
-	    private RadioButton onlyFilesNamesRadioButton;
+	@FXML
+	private RadioButton onlyFilesNamesRadioButton;
 
-	    @FXML
-	    private RadioButton fullPathNames;
+	@FXML
+	private RadioButton fullPathNames;
+	@FXML
+	private TextArea fileSearchTextArea;
+	private DirectoryTree directoryTree;
 
-	  	@FXML
-	    void initialize() {
-	  	
-	    	
-	        
-	    }
-	  	private 
-	  	@FXML
-		void handleChooseDirectory(ActionEvent event) {
-	  		
+	@FXML
+	void initialize() {
+		this.directoryTree = new DirectoryTree();
+	}
 
-		} 
+	@FXML
+	void handleChooseDirectory(ActionEvent event) {
+		DirectoryChooser name = new DirectoryChooser();
 
-	  	    
+		File folder = name.showDialog(((Node) event.getTarget()).getScene().getWindow());
+
+		if (folder != null) {
+			this.directoryTree.clearList();
+			this.directoryTree.addFiles(folder);
+			if (fullPathNames.isSelected()) {
+				for (String currName : this.directoryTree.getList()) {
+					this.fileSearchTextArea.setText(currName);
+				}
+				
+			}
+		}
+
+	}
 
 }
